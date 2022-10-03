50aaddadd629301d77425e171ff4f026744dc442
Henrique <78804989+Henrique-Coder@users.noreply.github.com>
2022-10-03 17:38:31 -0300

    Code cleanup

--- thumb.py
+++ thumb.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import requests
 from pytube import YouTube, extract
-from os import makedirs
 
 def format_title(title):
     new_title = ''
@@ -20,13 +18,8 @@ if st.button('Download Thumbnail'):
 
     yt_id = extract.video_id(url)
 
-    makedirs('Thumbnails', exist_ok=True)
     yt_thumbnail = f'https://img.youtube.com/vi/{yt_id}/maxresdefault.jpg'
-    r = requests.get(yt_thumbnail, allow_redirects=True)
-    open(f'Thumbnails\\{yt_fomatted_title}.jpg', 'wb').write(r.content)
 
     st.image(yt_thumbnail)
 
-    st.success('Thumbnail downloaded successfully (by FHDP)')
-
-    # https://youtu.be/T611mVOrPQg
+    st.success('The thumbnail was generated successfully! (Coder: FHDP)')
